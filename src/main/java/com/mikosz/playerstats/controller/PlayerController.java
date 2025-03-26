7849ba502eb4453402263d47722f1980fe822c4e
mikosz08 <ziolek.mikosz@protonmail.com>
2025-03-26 23:08:09 +0100

    Add annotations to Player's endpoints

--- src/main/java/com/mikosz/playerstats/controller/PlayerController.java
+++ src/main/java/com/mikosz/playerstats/controller/PlayerController.java
@@ -2,6 +2,7 @@ package com.mikosz.playerstats.controller;
 
 import com.mikosz.playerstats.model.Player;
 import com.mikosz.playerstats.service.PlayerService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,28 +21,33 @@ public class PlayerController {
         this.playerService = playerService;
     }
 
+    @Operation(summary = "Get all players")
     @GetMapping
     public List<Player> getAllPlayers() {
         return playerService.getAllPlayers();
     }
 
+    @Operation(summary = "Get player")
     @GetMapping("/{id}")
     public ResponseEntity<Player> getPlayerById(@PathVariable Long id) {
         return ResponseEntity.ok(playerService.getPlayerById(id));
     }
 
+    @Operation(summary = "Create new player")
     @PostMapping
     public ResponseEntity<Player> createPlayer(@Valid @RequestBody Player player) {
         Player created = playerService.createPlayer(player);
         return ResponseEntity.ok(created);
     }
 
+    @Operation(summary = "Update new player")
     @PutMapping("/{id}")
     public ResponseEntity<Player> updatePlayer(@PathVariable Long id, @Valid @RequestBody Player player) {
         Player updated = playerService.updatePlayer(id, player);
         return ResponseEntity.ok(updated);
     }
 
+    @Operation(summary = "Delete new player")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePlayer(@PathVariable Long id) {
         playerService.deletePlayer(id);
