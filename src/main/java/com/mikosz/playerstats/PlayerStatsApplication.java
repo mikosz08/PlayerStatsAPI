efdb0aaa377b174e48c8ece89a4bc68cf0963d6e
mikosz08 <ziolek.mikosz@protonmail.com>
2025-03-26 14:11:50 +0100

    [Init Commit] Project based on Spring Boot, Gradle and basic dependencies

---
+++ src/main/java/com/mikosz/playerstats/PlayerStatsApplication.java
@@ -0,0 +1,13 @@
+package com.mikosz.playerstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikosz/playerstats/PlayerStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikosz.playerstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
