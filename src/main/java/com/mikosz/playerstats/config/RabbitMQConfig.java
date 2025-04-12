074dc919435a77856b95e51fc9a8c58b85b36449
mikosz08 <ziolek.mikosz@protonmail.com>
2025-04-12 23:52:00 +0200

    Add RabbitMQ config and connection settings

---
+++ src/main/java/com/mikosz/playerstats/config/RabbitMQConfig.java
@@ -0,0 +1,34 @@
+package com.mikosz.playerstats.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${player.queue.name}")
+    private String queueName;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+}
